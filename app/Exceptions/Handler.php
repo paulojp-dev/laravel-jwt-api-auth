1914656e98da8294972e2777ffc1b5c213d6c9a0
João Paulo F <joao.paulo@zage.com.br>
2018-07-26 11:06:51 -0300

    adjusted when redirect for unauthenticated user

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -56,10 +56,12 @@ class Handler extends ExceptionHandler
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-
-        return redirect()->guest(route('login'));
+//        if ($request->expectsJson()) {
+//            return response()->json(['error' => 'Unauthenticated.'], 401);
+//        }
+//
+//        return redirect()->guest(route('login'));
+	
+	    return response()->json(['error' => 'Unauthenticated.'], 401);
     }
 }
